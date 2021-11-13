6eafebdbcd9488d935f6c338591311d6a32455aa
Daniel Kovacs <info@kovix.info>
2021-11-13 08:05:06 +0100

    Moved app1, app2

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,8 @@
+const setCookie = (str) => {
+  const now = new Date();
+  now.setTime(now.getTime() + 15*60*1000);
+  const sExpires = now.toUTCString();
+  document.cookie=`token=${str};expires=${sExpires};path=/`;
+}
 
+ export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,33 @@
 
+const setCookie = (name, value) => {
+    const now = new Date();
+    now.setTime(now.getTime() + 15*60*1000);
+    const sExpires = now.toUTCString();
+    document.cookie=`${name}=${value};expires=${sExpires};path=/`;
+  }
+
+setCookie('viewed', 5);  
+setCookie('uid', '354774631237');  
+setCookie('ssid', 'Bx55OWbHJ0Vt_IGIF');  
+
+const cookieHandler = {
+    getAll() {
+        return document.cookie.split('; ').reduce((all, val) => ({ ...all, [val.split('=')[0]]: val.split('=')[1]}), {});
+    },
+
+    toSessionStorage() {
+        const cookies = this.getAll();
+        for (let key in cookies) {
+            sessionStorage.setItem(key, cookies[key]);
+        }
+    },
+    flush() {
+        const cookies = this.getAll();
+        const sExpires = new Date(0).toUTCString();
+        for (let key in cookies) {
+            document.cookie=`${key}=;expires=${sExpires};path=/`;
+        }       
+    }
+};
+
+ export { cookieHandler };
